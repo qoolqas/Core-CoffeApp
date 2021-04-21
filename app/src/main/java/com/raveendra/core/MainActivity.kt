f6f1da1d1c21d9edff0b9e2f5b69575ce485b5bd
qoolqas <qoolqas@gmail.com>
2021-04-21 12:37:36 +0700

    add auth navigation

--- app/src/main/java/com/raveendra/core/MainActivity.kt
+++ app/src/main/java/com/raveendra/core/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+        supportActionBar?.hide()
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
--- app/src/main/java/com/raveendra/core/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/raveendra/core/ui/profile/ProfileViewModel.kt
@@ -1,10 +1,10 @@
-package com.raveendra.core.ui.notifications
+package com.raveendra.core.ui.profile
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class ProfileViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
